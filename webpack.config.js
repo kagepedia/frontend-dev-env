b228888cc0c3549e4fff7a9cf6585831fc8716ea
Kage Soichiro <k4gepedia@gmail.com>
2020-01-19 02:35:48 +0900

    pug sass js webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
 const path = require('path'); // ファイルパスの操作をするnode.jsのモジュール
 const webpack = require('webpack'); // webpack本体
 const ExtracktTextPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: ['./source/assets/js/main.js', './source/assets/scss/main.scss']
+        main: ['./source/assets/js/main.js', './source/assets/scss/main.scss', './source/template/index.pug']
     },
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'js/[name].bundle.js'
     },
+    devServer: {
+        port: 3000,
+    },
     module: {
         rules: [
+            { 
+                test: /\.pug$/,
+                use: {
+                    loader: 'pug-loader',
+                    options: {
+                        pretty: true
+                    }
+                }
+            },
             {
-                test:/.js$/,
+                test:/\.js$/,
                 exclude: '/node_modules/',
                 use: {
                     loader: 'babel-loader',
@@ -24,27 +37,30 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ExtracktTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                plugins: () => [
-                                    require('autoprefixer')({ browsers: ['last 2 versions'] }),
-                                ],
-                            },
-                        },
-                        {
-                            loader: 'sass-loader'
-                        },
-                    ]
-                }),
-            },
-        ],
-        plugins: [
-            new ExtracktTextPlugin({
-                filename: './css/[name].css'
-            })
+                use: [
+                    ExtracktTextPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            //minimize: true,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            }
         ]
-    }
+    },
+    plugins: [
+        new ExtracktTextPlugin({
+            filename: './css/[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './source/template/index.pug'
+        })
+    ]
 }
