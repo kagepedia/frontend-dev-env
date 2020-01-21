1664eccc00a6a8cb833368cbdc1e1df305166b0c
Kage Soichiro <k4gepedia@gmail.com>
2020-01-22 02:07:55 +0900

    pug compiler fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,129 @@
-const path = require('path'); // ファイルパスの操作をするnode.jsのモジュール
-const webpack = require('webpack'); // webpack本体
-const ExtracktTextPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const globule = require('globule');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: {
-        main: ['./source/assets/js/main.js', './source/assets/scss/main.scss', './source/template/index.pug']
-    },
+const dir = {
+    src: path.join(__dirname, 'source'),
+    public: path.join(__dirname, 'public')
+};
+
+const convertExtensions = {
+    pug: 'html',
+    scss: 'css',
+    js: 'js'
+};
+
+const mode = 'production'; //development:開発, production:本番
+const entry = {
+    pug: {},
+    scss: {},
+    js: {}
+};
+
+// ファイルの分類
+Object.keys(convertExtensions).forEach(from => {
+    const to = convertExtensions[from];
+    globule.find([`**/*.${from}`, `!**/_*.${from}`], { cwd: dir['src'] }).forEach(filename => {
+        let _output = filename.replace(new RegExp(`.${from}$`, 'i'), `.${to}`);
+        let _source = path.join(dir['src'], filename);
+        if (_output.indexOf('template/') !== -1) {
+            _output = _output.replace('template/', '');
+            entry['pug'][_output] = _source;
+            console.log('-------------------_output------------------------')
+            console.log(_output);
+            console.log('-------------------_source------------------------')
+            console.log(_source);
+            console.log('-------------------end------------------------')
+            /*
+            return new HtmlWebpackPlugin({
+                filename: filename.replace(new RegExp(`.${from}$`, 'i'), `.${to}`).replace(/(\.\/)?pug/, '.'),
+                template: `./${filename}`
+            })
+            */
+        }
+        if (_output.indexOf('assets/scss/') !== -1) {
+            _output = _output.replace('assets/scss/', 'assets/css/');
+            entry['scss'][_output] = _source;
+        }
+        if (_output.indexOf('assets/js/') !== -1) {
+            _output = _output.replace('assets/js/', 'assets/js/');
+            entry['js'][_output] = _source;
+        }
+    });
+});
+
+// pug
+const pugConfig = {
+    mode: mode,
+    entry: entry['pug'],
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'js/[name].bundle.js'
-    },
-    devServer: {
-        port: 3000,
+        filename: '[name]',
+        publicPath: '/',
+        path: dir['public']
     },
     module: {
         rules: [
-            { 
+            {
                 test: /\.pug$/,
-                use: {
-                    loader: 'pug-loader',
-                    options: {
-                        pretty: true
+                use: ['pug-loader', 'apply-loader']
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name]')
+    ],
+    cache: true
+};
+
+// sass
+const scssConfig = {
+    mode: mode,
+    entry: entry['scss'],
+    output: {
+        filename: '[name]',
+        publicPath: 'assets/',
+        path: dir['public']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract([
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
                     }
-                }
-            },
+                ])
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name]')
+    ],
+    cache: true
+};
+
+// js
+const jsConfig = {
+    mode: mode,
+    entry: entry['js'],
+    output: {
+        filename: '[name]',
+        publicPath: 'assets/',
+        path: dir['public']
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
             {
                 test:/\.js$/,
                 exclude: '/node_modules/',
@@ -34,33 +133,13 @@ module.exports = {
                       presets: ['babel-preset-env']
                     }
                 }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    ExtracktTextPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false,
-                            //minimize: true,
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                    }
-                ]
             }
         ]
     },
     plugins: [
-        new ExtracktTextPlugin({
-            filename: './css/[name].css'
-        }),
-        new HtmlWebpackPlugin({
-            filename: './index.html',
-            template: './source/template/index.pug'
-        })
-    ]
-}
+        new webpack.optimize.AggressiveMergingPlugin()
+    ],
+    cache: true
+};
+
+module.exports = [scssConfig, jsConfig, pugConfig];
